dfa7e554e4558d9d717fbc6943ea7d8be6387a20
dereypl <mpasich.pk@gmail.com>
2019-07-19 14:03:50 +0200

    add form and handle date-picker

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import './AddTask.css'
 
 class AddTask extends Component {
-    state = {}
+
+    minDate = new Date().toISOString().slice(0, 10);
+    state = {
+        text: '',
+        checked: false,
+        date: this.minDate
+    }
+
+    handleDate = e => {
+        this.setState({
+            date: e.target.value
+        })
+    }
+
+
     render() {
+
+        let maxDate = this.minDate.slice(0, 4) * 1 + 1;
+        maxDate = maxDate + "-12-31";
+
         return (
             <div className="form">
                 <input type="text" placeholder="dodaj zadanie" value={this.state.text} />
-                <input type="checkbox" checked={this.state.checked} />
+                <input type="checkbox" checked={this.state.checked} id="important" />
+                <label htmlFor="important">Priotytet</label>
+                <br />
+                <label htmlFor="date">Deadline</label>
+                <input type="date" value={this.state.date} min={this.minDate} max={maxDate} onChange={this.handleDate} />
+                <br />
+                <button>Dodaj</button>
             </div>
         );
     }
 }
+
 
 export default AddTask;
