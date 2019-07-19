059bba502a35dec82be52e646124d79feefba666
dereypl <mpasich.pk@gmail.com>
2019-07-19 11:29:34 +0200

    Updates

---
+++ precache-manifest.b56af2de6051f66694e448ecfe669035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422f77aea9a984655688405eeae4d2c0",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "26435afaedb983d8ff43",
+    "url": "/todoapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/todoapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "26435afaedb983d8ff43",
+    "url": "/todoapp/static/js/main.bbf3598e.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
